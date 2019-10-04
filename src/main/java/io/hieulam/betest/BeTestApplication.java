c550c8d0f7dd18729d892135f47a129ae70627f1
Hieu Lam <lamtrhieu@gmail.com>
2019-10-04 15:30:32 +0700

    Project setup

---
+++ src/main/java/io/hieulam/betest/BeTestApplication.java
@@ -0,0 +1,13 @@
+package io.hieulam.betest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeTestApplication.class, args);
+	}
+
+}
